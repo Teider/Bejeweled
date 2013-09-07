fea947b760567265c8b42714c321fc1dfc29991f
Marcelo Teider Lopes <teiderlopes@gmail.com>
2013-09-07 00:17:47 -0300

    Initial basic game loop test.

---
+++ src/core/game.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "game.h"
+
+namespace Bejeweled {
+
+Game::loop() {
+	while(true) {
+		cout << "Teste" << std::endl; 
+		break;
+	}
+}
+
+}
+
---
+++ src/core/game.h
@@ -0,0 +1,13 @@
+#ifndef BEJEWELED_CORE_GAME_H_
+#define BEJEWELED_CORE_GAME_H_
+
+namespace Bejeweled {
+
+public class Game {
+	public:
+		void loop();
+}
+
+}
+
+#endif //BEJEWELED_CORE_GAME_H_
