896b8fd68cdf36e0236ed1d6cdcc50eb3d91203e
Marcelo Teider Lopes <teiderlopes@gmail.com>
2013-09-07 01:30:18 -0300

    Implementation of graphics config.

---
+++ src/graphics/impl/SDL/graphics/SDLconfig.cpp
@@ -0,0 +1,16 @@
+#include <SLD2/SDL.h>
+
+#include "graphics/config.h"
+
+namespace bejeweled {
+namespace graphics {
+
+Config::Config(uint32_t flags) {
+  if (SDL_Init(flags) != 0) {
+    throw std::runtime_error(SDL_GetError());
+  }
+}
+
+} // namespace graphics
+} // namespace bejeweled
+
