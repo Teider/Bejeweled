da2bf317c19a8c719fc40ee46a5739c801e3ec23
Marcelo Teider Lopes <teiderlopes@gmail.com>
2013-09-07 01:16:54 -0300

    Created graphics config interface.

---
+++ src/graphics/include/config.h
@@ -0,0 +1,18 @@
+#ifndef BEJEWELED_GRAPHIC_CONFIG_H_
+#define BEJEWELED_GRAPHIC_CONFIG_H_
+
+#include <cstdint>
+
+namespace Bejeweled {
+
+namespace Graphic {
+
+class Config {
+  public:
+
+    Config(Uint32 flags);
+
+}
+
+#endif //BEJEWELED_GRAPHIC_CONFIG_H_
+
