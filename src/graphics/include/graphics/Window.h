79a4735aaf4893f9f4bd730f526dc8d7be55b628
lucascp <lucascpaiva@gmail.com>
2013-09-07 23:16:21 -0300

    breaking build

--- src/graphics/include/graphics/Window.h
+++ src/graphics/include/graphics/Window.h
@@ -3,18 +3,25 @@
 
 #include <string>
 #include <cstdint>
+#include <memory>
 
 namespace bejeweled {
 namespace graphics {
 
 class Window {
 public:
-  Window(std::string, int x, int y, int w, int h, uint32_t flags);
+  virtual ~Window() {};
 
 protected:
-  ~Window();
+  Window() {};
+
+private:
+  Window(const Window&) = delete;
+  Window& operator=(const Window&) = delete;
 
 };
+
+std::unique_ptr<Window> WindowFactory(std::string name, int x, int y, int w, int h, uint32_t flags);
 
 } // namespace graphics
 } // namespace bejeweled
