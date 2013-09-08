1945f34cb8d91236fd5e781f707d19376ebd5bd7
lucascp <lucascpaiva@gmail.com>
2013-09-08 09:49:47 -0300

    missing files

---
+++ src/graphics/impl/SDL/graphics/SDLWindow.cpp
@@ -0,0 +1,17 @@
+#include <SDL.h>
+
+#include "graphics/SDLWindow.h"
+
+namespace bejeweled {
+namespace graphics {
+
+SDLWindow::SDLWindow(const std::string &name, int x, int y, int w, int h, uint32_t flags) {
+  ptr_ = std::unique_ptr<SDL_Window>(SDL_CreateWindow(name.c_str(), x, y, w, h, flags));
+}
+  
+std::unique_ptr<Window> WindowFactory(std::string name, int x, int y, int w, int h, uint32_t flags) {
+  return std::unique_ptr<Window>(new SDLWindow(name, x, y, w, h, flags));
+}
+
+} // namespace graphics
+} // namespace bejeweled
---
+++ src/graphics/impl/SDL/graphics/SDLWindow.h
@@ -0,0 +1,27 @@
+#ifndef BEJEWELED_GRAPHICS_SDLWINDOW_H_
+#define BEJEWELED_GRAPHICS_SDLWINDOW_H_
+
+#include <string>
+#include <cstdint>
+#include <memory>
+
+#include "graphics/Window.h"
+
+class SDL_Window;
+
+namespace bejeweled {
+namespace graphics {
+
+class SDLWindow : Window {
+public:
+  SDLWindow(const std::string &name, int x, int y, int w, int h, uint32_t flags);
+
+private:
+  std::unique_ptr<SDL_Window> ptr_;
+
+};
+
+} // namespace graphics
+} // namespace bejeweled
+
+#endif // BEJEWELED_GRAPHICS_SDLWINDOW_H_
