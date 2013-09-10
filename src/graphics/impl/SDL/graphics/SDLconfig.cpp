6386340f1e15c782c51e05b494fe2ac49ae9d3a9
lucascp <lucascpaiva@gmail.com>
2013-09-10 18:48:26 -0300

    add ~temporary~ config flags

--- src/graphics/impl/SDL/graphics/SDLconfig.cpp
+++ src/graphics/impl/SDL/graphics/SDLconfig.cpp
@@ -6,19 +6,28 @@
 namespace bejeweled {
 namespace graphics {
 
-Config::Config(uint32_t flags) {
-  if (SDL_Init(flags) != 0) {
+Config::Config(std::set<Config::Flags> flags) {
+  uint32_t sdl_flags = 0;
+  for(auto &f : flags)
+    sdl_flags |= Config::convert_.at(f);
+  if (SDL_Init(sdl_flags) != 0) {
     throw std::runtime_error(SDL_GetError());
   }
 }
 
 Config::Config() {
-  Config(SDL_INIT_EVERYTHING);
+  Config(std::set<Flags>{Flags::kInitEverything});
 }
 
 Config::~Config(){
   SDL_Quit();
 }
+
+const std::map<Config::Flags, uint32_t> Config::convert_ = {
+  { Config::Flags::kInitAudio, SDL_INIT_AUDIO},
+  { Config::Flags::kInitVideo, SDL_INIT_VIDEO},
+  { Config::Flags::kInitEvents, SDL_INIT_EVENTS},
+  { Config::Flags::kInitEverything, SDL_INIT_EVERYTHING}};
 
 } // namespace graphics
 } // namespace bejeweled
--- src/graphics/include/graphics/Config.h
+++ src/graphics/include/graphics/Config.h
@@ -1,17 +1,30 @@
 #ifndef BEJEWELED_GRAPHICS_CONFIG_H_
 #define BEJEWELED_GRAPHICS_CONFIG_H_
 
-#include <cstdint>
+#include <set>
+#include <map>
 
 namespace bejeweled {
 namespace graphics {
 
 class Config {
 public:
+  enum class Flags {
+	kInitAudio,
+	kInitVideo,
+	kInitJoystick,
+	kInitEvents,
+	kInitEverything
+  };
+
   Config();
-  Config(uint32_t flags);
+  Config(std::set<Flags> flags);
   ~Config();
- 
+private:
+  //FIXME: remove this from here. Config should be an interface with pure virtual methods,
+  //      the impl should be a subclass, and this map should only be there.
+  static const std::map<Flags, uint32_t> convert_;
+
 };
 
 } // namespace graphics
