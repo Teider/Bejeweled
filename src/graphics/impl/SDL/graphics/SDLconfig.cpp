2f59bf3538ead25295e84689f4a89f89152904ba
lucascp <lucascpaiva@gmail.com>
2013-09-10 18:51:27 -0300

    lets be less verbose

--- src/graphics/impl/SDL/graphics/SDLconfig.cpp
+++ src/graphics/impl/SDL/graphics/SDLconfig.cpp
@@ -6,10 +6,10 @@
 namespace bejeweled {
 namespace graphics {
 
-Config::Config(std::set<Config::Flags> flags) {
+Config::Config(std::set<Flags> flags) {
   uint32_t sdl_flags = 0;
   for(auto &f : flags)
-    sdl_flags |= Config::convert_.at(f);
+    sdl_flags |= convert_.at(f);
   if (SDL_Init(sdl_flags) != 0) {
     throw std::runtime_error(SDL_GetError());
   }
@@ -24,10 +24,10 @@ Config::~Config(){
 }
 
 const std::map<Config::Flags, uint32_t> Config::convert_ = {
-  { Config::Flags::kInitAudio, SDL_INIT_AUDIO},
-  { Config::Flags::kInitVideo, SDL_INIT_VIDEO},
-  { Config::Flags::kInitEvents, SDL_INIT_EVENTS},
-  { Config::Flags::kInitEverything, SDL_INIT_EVERYTHING}};
+  { Flags::kInitAudio, SDL_INIT_AUDIO },
+  { Flags::kInitVideo, SDL_INIT_VIDEO },
+  { Flags::kInitEvents, SDL_INIT_EVENTS },
+  { Flags::kInitEverything, SDL_INIT_EVERYTHING }};
 
 } // namespace graphics
 } // namespace bejeweled
