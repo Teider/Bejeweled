6cba4af7bf5313ab3a3144467204d30744731998
lucascp <lucascpaiva@gmail.com>
2013-09-10 22:20:54 -0300

    Add fixme message for SDLSurface

--- src/graphics/impl/SDL/graphics/SDLSurface.cpp
+++ src/graphics/impl/SDL/graphics/SDLSurface.cpp
@@ -7,6 +7,7 @@ SDLSurface::SDLSurface(SDL_Surface *ptr)
 	: ptr_(ptr, SDL_FreeSurface) {}
   
 std::unique_ptr<Surface> SurfaceFactory(std::string name) {
+  //FIXME: there should be a check for errors here
   return std::unique_ptr<Surface>(new SDLSurface(SDL_LoadBMP(name.c_str())));
 }
 
