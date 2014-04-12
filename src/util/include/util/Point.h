4f51d2bad959cb5d334cf976127a1e39a7d4597a
lucascp <lucascpaiva@gmail.com>
2014-04-12 20:17:08 -0300

    Add sum operators to Point and Rectangle

--- src/util/include/util/Point.h
+++ src/util/include/util/Point.h
@@ -6,9 +6,21 @@ namespace util {
 
 struct Point {
   Point(int x, int y) : x_(x), y_(y) {}
+
+  Point& operator+=(const Point &rhs) {
+    x_ += rhs.x_;
+    y_ += rhs.y_;
+    return *this;
+  }
+
   int x_;
   int y_;
 };
+
+inline Point operator+(Point lhs, const Point &rhs) {
+  lhs += rhs;
+  return lhs;
+}
 
 } // namespace util
 } // namespace bejeweled
